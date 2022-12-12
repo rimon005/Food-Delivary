35711deb187201bd0ecaa3ed0b8fd7f3ff2cfb4b
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-12-12 23:59:32 +0600

    add hero section

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import Image from 'next/image'
+import Logo from '../assets/Logo.png'
+import {UilFacebook , UilGithub , UilLinkedin } from '@iconscout/react-unicons'
+import css from '../styles/Footer.module.css'
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className={css.container}>
+            <span>ALL RIGHT RESERVED</span>
+            <div className={css.social}>
+                <UilFacebook size={54} />
+                <UilGithub  size={54}/>
+                <UilLinkedin size={54} />
+            </div>
+            <div className={css.logo}>
+                <Image src={Logo} alt='' height={50} width={50} />
+                <span>Fudo</span>
+            </div>
         </div>
     );
 };
--- Components/Header.js
+++ Components/Header.js
@@ -2,13 +2,30 @@ import React from 'react';
 import css from '../styles/Header.module.css'
 import Image from 'next/image';
 import Logo from '../assets/Logo.png'
+import {UilShoppingBag} from '@iconscout/react-unicons'
 const Header = () => {
     return (
-        <div className={css.header}> 
+        <div className={css.header}>
             {/* Logo side */}
-           <div className={css.logo}>
-            <Image src={Logo} alt='' />
-           </div>
+            <div className={css.logo}>
+                <Image src={Logo} alt='' height={50} width={50} />
+                <span>Fudo</span>
+            </div>
+            {/* Menu Side  */}
+            <ul className={css.menu}>
+                <li>Home</li>
+                <li>Menu</li>
+                <li>Contact</li>
+            </ul>
+            {/* Right Side */}
+            <div className={css.rightSide}>
+               <div className={css.cart}>
+                <UilShoppingBag size={35} color="#2E2E2E" />
+                    <div className={css.badge}>
+                        1
+                    </div>
+               </div>
+            </div>
         </div>
     );
 };
